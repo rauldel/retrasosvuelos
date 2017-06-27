5cbe626fa1c32a01107f3693df6de06700126aa5
Raúl del Río <rauldel@alumni.uv.es>
2017-06-27 19:53:08 +0200

    Subida con la version actualizada del angular-cli

--- src/app/contacto/contacto.component.ts
+++ src/app/contacto/contacto.component.ts
@@ -15,7 +15,7 @@ export class ContactoComponent implements OnInit {
    * Se ha usado esto: https://angular-maps.com/guides/getting-started/
    */
   lat: number = 39.4867634;
-  lng: number =  -0.4492538;
+  lng: number = -0.4492538;
   zoom: number = 17;
   tipoMapa: string = "hybrid";
 
